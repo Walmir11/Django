c7fbd77e5a92edaff77ae7c27fef243f58e21047
Walmir11 <walmirneto1990@gmail.com>
2025-01-15 09:48:26 -0300

    Adicionando view com dados de carro fixo

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def car_view(request):
+    return render(request,
+                  'cars.html',
+                  {'cars': {'model': 'Ferrari', 'year': 2021, 'color': 'red'}})
