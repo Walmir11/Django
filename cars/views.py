b5ce1b3593edbb23c3bd72169efb3f4b9003cb6d
Walmir11 <walmirneto1990@gmail.com>
2025-04-24 07:15:12 -0300

    Visualizando comunicação request

--- cars/views.py
+++ cars/views.py
@@ -4,7 +4,10 @@ from cars.models import Car
 # Create your views here.
 def car_view(request):
     # cars = Car.objects.filter(brand__name='Chevrolet')
-    cars = Car.objects.filter(model__icontains='onix')
+    # cars = Car.objects.filter(model__icontains='onix')
+    print(request.GET)
+
+    cars = Car.objects.all()
 
     return render(request,
                   'cars.html',
