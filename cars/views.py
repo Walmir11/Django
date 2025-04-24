55db6067ab320145b8a5831ec14beb35981dd910
Walmir11 <walmirneto1990@gmail.com>
2025-04-23 22:20:30 -0300

    Adicionando consultas ao banco de dados

--- cars/views.py
+++ cars/views.py
@@ -3,8 +3,8 @@ from cars.models import Car
 
 # Create your views here.
 def car_view(request):
-    cars = Car.objects.all()
-    print(cars)
+    # cars = Car.objects.filter(brand__name='Chevrolet')
+    cars = Car.objects.filter(model__icontains='onix')
 
     return render(request,
                   'cars.html',
