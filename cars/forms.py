1678717ece3f55a43c24eb55246fbc14977014bc
Walmir11 <walmirneto1990@gmail.com>
2025-05-07 19:31:47 -0300

    Adicionando validações

--- cars/forms.py
+++ cars/forms.py
@@ -7,6 +7,18 @@ class CarModelForm(forms.ModelForm):
         model = Car
         fields = '__all__'
 
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 15000:
+            self.add_error('value', f'Valor deve ser maior que 15000. Valor atual: {value}')
+        return value
+
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year > 1900:
+            self.add_error('year', f'Ano deve ser maior ao ano de 1900. Ano atual: {factory_year}')
+        return factory_year
+
 # class CarForm(forms.Form):
 #     model = forms.CharField(max_length=200)
 #     brand = forms.ModelChoiceField(Brand.objects.all())
