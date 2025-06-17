8080258d45002da35c47499edb1b11a24974ee48
Walmir11 <walmirneto1990@gmail.com>
2025-06-16 22:10:34 -0300

    Adicionando delete e update

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from cars.models import Car
 from cars.forms import CarModelForm
 
@@ -24,7 +25,18 @@ class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
 
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
 
 # Forma antiga
 # def car_view(request):
