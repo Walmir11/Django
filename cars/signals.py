642a7ab2ed0356ff54e0f7cf4aa85ac98cc36468
Walmir11 <walmirneto1990@gmail.com>
2025-07-07 23:07:36 -0300

    Adicionando update aos signals

--- cars/signals.py
+++ cars/signals.py
@@ -1,22 +1,17 @@
-from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
+from django.db.models import Sum
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from cars.models import Car
+from cars.models import Car, CarInventory
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print(f"Pre-save signal triggered for {instance}")
-
+def car_inventory_update():
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(total_value=Sum('value'))['total_value']
+    CarInventory.objects.create(cars_count=cars_count, cars_value=cars_value)
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print(f"Post-save signal triggered for {instance}")
-
-
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    print(f"Pre_delete signal triggered for {instance}")
-
+    car_inventory_update()
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    print(f"Post_delete signal triggered for {instance}")
+    car_inventory_update()
